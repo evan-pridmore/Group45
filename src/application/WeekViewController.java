8fc56c0bc072d7fefd4e5ca6be2f34e536485d39
aukur <allanhabar@gmail.com>
2022-11-20 00:17:59 -0700

    ADD: Basic structure for menu bar events.

--- src/application/WeekViewController.java
+++ src/application/WeekViewController.java
@@ -1,6 +1,7 @@
 package application;
 
 import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
 import javafx.scene.control.Menu;
 import javafx.scene.layout.VBox;
 
@@ -35,4 +36,24 @@ public class WeekViewController {
     
     @FXML
     private VBox UpcomingWindow;
+    
+    @FXML
+    private void presentNewEventMenu(ActionEvent newEvent) {
+    	System.out.println("New Event Menu");
+    }
+    
+    @FXML
+    private void presentRemoveEventMenu(ActionEvent removeEvent) {
+    	System.out.println("Remove Event Menu");
+    }
+    
+    @FXML
+    private void presentSwitchUserMenu(ActionEvent switchUserEvent) {
+    	System.out.println("Switch User Menu");
+    }
+    
+    @FXML
+    private void logOut(ActionEvent logOutEvent) {
+    	System.out.println("Log out");
+    }
 }
