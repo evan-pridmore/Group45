5fa1cd127058f708d99dcb9bacb15eb5e16cbe3d
evan-pridmore <evan.pridmore@ucalgary.ca>
2022-11-21 20:55:19 -0700

    Added Basic Serialization (Attempt) to Save User Data
    Changes:
    --> Re-worked User class to deal with saving of user data to independent
    save (.ser) files via serializeUser and deserializeUser methods.
    --> Added expanded capacity of attemptLogin method in
    LoginViewController to utilize newly added deserializeUser method in
    User. (username/password entered in via GUI is used as arguments for
    deserializeUser in an attempt to read User data from user save (.ser
    file).
    --> Added loginErrorLabel in loginView to display errors.
    --> Added UserDoesNotExistException to indicate that a user does not
    exist as a save (.ser) file.
    --> Deprecated LoginDataOLD class (kept for future reference only).
    
    Current State:
    --> The user can attempt to log in, and arguments from the textFields
    are passed to the deserializeUser method, but since no user save (.ser)
    files exist, we cannot access any pre-existing data.
    --> This will throw a UserDoesNotExistException, which is displayed in
    the GUI and console.
    
    To-do:
    --> Add user creation functionality to GUI (via new .fxml file?).
    
    I am hoping that through serialization we can also save event data,
    which would be great for retaining user data throughout opening and
    closing the application.

--- src/application/LoginViewController.java
+++ src/application/LoginViewController.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
@@ -13,9 +15,27 @@ public class LoginViewController {
     private TextField loginPassword;
 	
 	@FXML
-	void attemptLogin() {
+	private Label loginErrorLabel;
+		
+	@FXML
+	public void attemptLogin() throws IOException {
+		loginErrorLabel.setText("");
 		System.out.println(String.format("Attempted Login:%n--> Username: '%s'%n--> Password: '%s'", loginUsername.getText(), loginPassword.getText()));
-		// Requires File ID module to access and compare stored login information? i.e., use a for-loop to scan through a list of stored usernames and correlating passwords?
-		// Look into using HashMaps to create a key:value pair (username:password) pair, wrapped with a try/catch statement.
-	}
+		
+		try {
+			User.deserializeUser(loginUsername.getText());
+		} catch (ClassNotFoundException | IOException e) {
+			// This should be a custom exception
+			System.out.println(String.format("ERROR (attemptLogin): Cannot deserialize user '%s'. ", loginUsername.getText()));
+			System.out.println(e.getMessage());
+			
+		} catch (UserDoesNotExistException udne) {
+			System.out.println(String.format("ERROR (attemptLogin): User '%s' does not exist.", loginUsername.getText()));
+			loginErrorLabel.setText(udne.getMessage());
+		}
+	} 
+	
 }
+	
+	// Requires File ID module to access and compare stored login information? i.e., use a for-loop to scan through a list of stored usernames and correlating passwords?
+	// Look into using HashMaps to create a key:value pair (username:password) pair, wrapped with a try/catch statement.
--- src/application/User.java
+++ src/application/User.java
@@ -1,14 +1,20 @@
 package application;
 
-public class User {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class User implements Serializable {
+
 	private String username;
 	private String password;
 	
-	User(){}
-	
-	User(String usernameInput, String passwordInput){
-		setUsername(usernameInput);
-		setPassword(passwordInput);
+	User(String usernameInput, String passwordInput) {
+		
 	}
 	
 	public String getUsername() {
@@ -19,13 +25,67 @@ public class User {
 		return new String(password);
 	}
 	
-	public void setUsername(String input) {
-		if (input != null)
-			username = new String(input);
+	public void setUsername(String usernameInput) {
+		if (usernameInput != null) {
+			username = usernameInput;
+		} else {
+			System.out.println(String.format("ERROR (setUsername): Cannot set username to '%s'.", usernameInput));
+			//throw custom exception (invalidUsernameInput)
+		}
 	}
 	
-	public void setPassword(String input) {
-		if (input != null)
-			password = new String(input);
+	public void setPassword(String passwordInput) {
+		if (passwordInput != null) {
+			password = passwordInput;
+		} else {
+			System.out.println(String.format("ERROR (setPassword): Cannot set password to '%s'.", passwordInput));
+			//throw custom exception (invalidUsernameInput)
+		}
 	}
+	
+	public static void serializeUser(User inputUser) throws IOException {
+		try {
+			FileOutputStream fileOut = new FileOutputStream("loginData" + inputUser + ".ser");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(inputUser);
+			out.close();
+			fileOut.close();
+			System.out.println("(User) Saved login data '%s', '%s' to 'loginData.ser'.");
+			
+		} catch (IOException ioe) {
+			System.out.println("ERROR (serializeUser): Cannot save user data to loginData.ser");
+		}
+	}
+	
+	public static User deserializeUser(String inputUsername) throws ClassNotFoundException, IOException, UserDoesNotExistException {
+		User outputUser = null;
+		
+		File loginData = new File("loginData" + inputUsername + ".ser");
+				
+		if (loginData.exists() && loginData.canRead()) {
+			System.out.println(String.format("deserializeUser: data for username '%s' from file '%s'", inputUsername, loginData));
+			try {
+				FileInputStream fileIn = new FileInputStream(loginData);
+				ObjectInputStream in = new ObjectInputStream(fileIn);
+				outputUser = (User) in.readObject();
+				
+				in.close();
+				fileIn.close();
+				
+			} catch (IOException ioe) {
+				System.out.println(String.format("ERROR (deserializeUser): Cannot read user data from '%s'", loginData));
+				System.out.println(ioe.getMessage());
+				
+			} catch (ClassNotFoundException cnfe) {
+				System.out.println(String.format("ERROR (deserializeUser): Cannot read user data from '%s'", loginData));
+				System.out.println(cnfe.getMessage());
+				
+			}
+		} else {
+			throw new UserDoesNotExistException(String.format("ERROR (deserializeUser): file '%s' either does not exist or cannot be read.", loginData));
+		}
+		
+		return outputUser;
+	}
+	
 }
