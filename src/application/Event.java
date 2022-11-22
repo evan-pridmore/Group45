27cfe2001991a3990ceb489be03bffc3198d70df
aukur <allanhabar@gmail.com>
2022-11-22 00:59:37 -0700

    ADD: Basic structure to Event class.

--- src/application/Event.java
+++ src/application/Event.java
@@ -1,5 +1,32 @@
 package application;
 
-public class Event {
+import java.util.Date;
 
+public class Event extends TimeUnit{
+	private String name;
+	private int[] colour = new int[3];
+	
+	Event(Date start, Date end, String aName, int[] aColour) throws InvalidColourException {
+		super(start, end);
+		setName(aName);
+		setColour(aColour);
+	}
+	
+	public void setName(String givenName) {
+		name = givenName;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setColour(int[] newColour) throws InvalidColourException {
+		if (newColour.length != 3)
+			throw new InvalidColourException();
+		colour = newColour;
+	}
+	
+	public int[] getColour() {
+		return colour;
+	}	
 }
