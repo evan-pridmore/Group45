06385728917e786e198cc78964ac593f7e56565b
evan-pridmore <evan.pridmore@ucalgary.ca>
2022-11-22 19:55:28 -0700

    Added Scene-Switching Functionality to Login
    Currently, the user is able to create new user save data and log into a
    specific user account. Logging in will switch the scene to CalendarView.
    
    Changes:
    --> Added InvalidPasswordException
    --> Added Scene change functionality to Main
    --> Added JavaDoc and commenting.
    --> Added basic layout for CalendarView
    --> Added unfinished CalendarViewController

---
+++ src/application/CalendarViewController.java
@@ -0,0 +1,17 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
+
+public class CalendarViewController {
+	
+	@FXML
+	private VBox upcomingEventsVBox;
+	
+	@FXML
+	private static VBox dayViewVBox;
+	
+	//Unfinished.
+	
+}
--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module Group45Project {
 	requires javafx.fxml;
 	requires javafx.controls;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
