70a3f6bebddafb0f4d2ecee9b04bdb319e57098e
evan-pridmore <evan.pridmore@ucalgary.ca>
2022-11-27 22:17:59 -0700

    Restarting Restructuring Process to Resolve JavaFXML Null Node Issue

--- src/application/ApplicationController.java
+++ src/application/ApplicationController.java
@@ -1,12 +1,5 @@
 package application;
 
-import java.io.FileInputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -17,21 +10,28 @@ public class ApplicationController {
 	private static CalendarViewController calendarController;
 	private static EventManagementController eventManagementController;
 	
-	private static Stage applicationStage;
+	private static Stage appStage;
+	private static Scene appScene;
 	
-	public SimpleDateFormat DateFormatPhrase = new SimpleDateFormat("EEEE, MMMM dd");
 	
 	private static User currentUser; 
-	private Date selectedDate = new Date();
-	
-	public Stage getApplicationStage() {
-		return applicationStage;
+
+	public Stage getAppStage() {
+		return appStage;
 	}
 	
-	public void setApplicationStage(Stage stageInput) {
-		applicationStage = stageInput;
+	public void setAppStage(Stage stageInput) {
+		appStage = stageInput;
 	}
 	
+	public static Scene getAppScene() {
+		return appScene;
+	}
+
+	public static void setAppScene(Scene appScene) {
+		ApplicationController.appScene = appScene;
+	}
+
 	public User getCurrentUser() {
 		return currentUser;
 	}
@@ -40,21 +40,8 @@ public class ApplicationController {
 		currentUser = userInput;
 	}
 	
-	public String getSelectedDate() {
-		DateFormatPhrase.format(selectedDate);
-		return DateFormatPhrase.format(selectedDate);
-	}
-	
-	public void setSelectedDate(Date dateInput) {
-		selectedDate = dateInput;
-	}
-	
 	public ApplicationController getApplicationController() {
 		return appController;
-	}
-	
-	public void setApplicationController() {
-		appController = this;
 	}
 	
 	public void setLoginViewController(LoginViewController controllerInput) {
@@ -64,14 +51,6 @@ public class ApplicationController {
 	public void setCalendarViewController(CalendarViewController controllerInput) {
 		calendarController = controllerInput;
 	}
-	
-	public EventManagementController getEventManagementController() {
-		return eventManagementController;
-	}
-
-	public void setEventManagementController(EventManagementController controllerInput) {
-		eventManagementController = controllerInput;
-	}
 
 		// Opens a new application window depending on the user that has logged-in from the loginView.
 		// Needs to be static and take in a user as an argument
@@ -79,43 +58,15 @@ public class ApplicationController {
 	    	System.out.println("initializeCalendarView: Attempting to initialize CalendarView...");
 
 			try {
-				FXMLLoader loader = new FXMLLoader();
-	 			Parent root = loader.load(new FileInputStream("src/application/FXML/CalendarView.fxml"));
-				loader.setController(calendarController);
-				
-				System.out.println("initializeCalendarView: Contoller set to '" + loader.getController() + "'.");
-	 			
-	 			Scene scene = new Scene(root);
-	 			applicationStage.setScene(scene);
-	 			applicationStage.setTitle(currentUser.getUsername() + "'s Calendar");
-	 			applicationStage.centerOnScreen();
-	 			
-	 			System.out.println(calendarController);
-	 			calendarController.updateDateLabels();
-	 			
-	  			applicationStage.show();
 	  				  						
 			} catch (Exception e) { e.printStackTrace(); }
-			
-			System.out.println("Today's date is: " + selectedDate);
-			System.out.println("initializeCalendarView: CalendarView successfully initialized.");
+
 		}
 		
 		public void initializeLoginView() {
 	    	System.out.println("initializeLoginView: Attempting to initialize LoginView...");
 
 			try {
-				FXMLLoader loader = new FXMLLoader();
-	 			Parent root = loader.load(new FileInputStream("src/application/FXML/LoginView.fxml"));
-				loader.setController(loginController);
-
-				System.out.println("initializeLoginView: Contoller set to '" + loader.getController() + "'.");
-				
-	 			Scene scene = new Scene(root);
-	 			applicationStage.setScene(scene);
-	 			applicationStage.setTitle("Login");
-	 			applicationStage.centerOnScreen();
-	 			applicationStage.show();
 							
 			} catch (Exception e) { e.printStackTrace(); }
 			
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,6 @@
 package application;
-	
-import java.io.FileInputStream;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 
 
@@ -13,45 +10,6 @@ public class Main extends Application {
 		System.out.println("Main: Starting application...");
 
 		try {
-			// Initializing ApplicationController
-			System.out.print("Main: Initializing ApplicationController...");
-			ApplicationController appController = new ApplicationController();
-			appController.setApplicationController();
-			System.out.println("     Done.");
-
-			// Initializing LoginViewController
-			System.out.print("Main: Initializing LoginViewController...");
-			FXMLLoader loginLoader = new FXMLLoader();
-			loginLoader.load(new FileInputStream("src/application/FXML/LoginView.fxml"));
-			LoginViewController loginController = loginLoader.getController();
-			appController.setLoginViewController(loginController);
-			System.out.println("     Done.");
-			
-			// Initializing CalendarViewController
-			System.out.print("Main: Initializing CalendarViewController...");
-			FXMLLoader calendarLoader = new FXMLLoader();
-			calendarLoader.load(new FileInputStream("src/application/FXML/CalendarView.fxml"));
-			CalendarViewController calendarController = calendarLoader.getController();
-			appController.setCalendarViewController(calendarController);
-			System.out.println("     Done.");
-			
-			// Initializing EventManagementController
-			System.out.print("Main: Initializing EventManagementController... ");
-			FXMLLoader eventManagerLoader = new FXMLLoader();
-			eventManagerLoader.load(new FileInputStream("src/application/FXML/EventsViewerView.fxml"));
-			EventManagementController eventManagerController = eventManagerLoader.getController();
-			appController.setEventManagementController(eventManagerController);
-			System.out.println("     Done.");
-			
-			System.out.println("");
-			
-			appController.setApplicationStage(primaryStage);
-
-			// Initializing application GUI...
-			appController.initializeLoginView();
-			
-	    	System.out.println(String.format("Main: initialized controllers.%n--> appController = %s %n--> loginController = %s %n-->calendarController = %s %n--> currentUser = %s", appController, loginController, calendarController, eventManagerController));
-	    	System.out.println("");
 			
 		} catch(Exception e) { e.printStackTrace(); }
 	}
