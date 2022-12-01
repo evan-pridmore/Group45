bfbce4edcd41681dd18041eece56ac029071471e
evan-pridmore <evan.pridmore@ucalgary.ca>
2022-11-30 18:14:20 -0700

    Re-added Features from Evan_5 after Merge Error

--- src/application/CalendarViewController.java
+++ src/application/CalendarViewController.java
@@ -3,8 +3,13 @@ package application;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
+import java.util.ArrayList;
 import java.util.Date;
 
+import application.TimeUnits.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
@@ -26,8 +31,7 @@ import javafx.scene.shape.Rectangle;
  */
 public class CalendarViewController extends ApplicationController  {
 	
-public SimpleDateFormat dateLabelFormat = new SimpleDateFormat("EEEE, MMMM dd");
-public Date currentDate = new Date();
+	public DateTimeFormatter dateLabelFormat = DateTimeFormatter.ofPattern("EEEE, MMMM dd");
 	
 	@FXML
 	private Menu EditMenu;
@@ -66,20 +70,20 @@ public Date currentDate = new Date();
     }
     
     @FXML
-    void addEventMenu(ActionEvent addEventEvent) throws FileNotFoundException, IOException {
+    void addEventMenu(ActionEvent addEventEvent) {
     	System.out.println("addEventMenu: Attempting to initialize EventManagerView...");
     	initializeEventMakerView();
     }
     
     @FXML
-    void manageEventsMenu(ActionEvent removeEventEvent) throws FileNotFoundException, IOException {
+    void manageEventsMenu(ActionEvent removeEventEvent) {
     	System.out.println("removeEventMenu: Attempting to remove event...");
     	initializeEventViewerView();
     }
     
     @FXML
     void openAdminPreferences(ActionEvent openAdminPreferencesEvent) {
-    	System.out.println("openAdminPreferences: ...");
+    	System.out.println("openAdminPreferences: Initializing tests...");
     	Rectangle testEvent = new Rectangle(200, 75, Color.LIGHTBLUE);
     	Rectangle testEvent2 = new Rectangle(200, 75, Color.LIGHTYELLOW);
     	Rectangle testEvent3 = new Rectangle(200, 75, Color.PINK);
@@ -92,7 +96,18 @@ public Date currentDate = new Date();
     	VBox.setMargin(testEvent2, upcomingEventsInsets);
     	VBox.setMargin(testEvent3, upcomingEventsInsets);
     	
+    	/*
+     	getCurrentUser().dumpEvents();
+     	
+     	TimedEvent AdminTestEvent = new TimedEvent(ZonedDateTime.now(), ZonedDateTime.now().plusHours(1), "AdminTest", Color.RED);
+ 		getCurrentUser().addEvent(AdminTestEvent);
+ 		User.serializeUser(getCurrentUser());
+ 		System.out.println("AdminTest: Event created with name '" + AdminTestEvent.getName() + "'.");
+     	*/
+    	
     	getCurrentUser().dumpEvents();
+    	
+		updateDayView();
     }
     
     /**A method that updates the GUI specifically in {@link CalendarView.fxml} through the {@link CalendarViewController} class. <p>
@@ -103,16 +118,60 @@ public Date currentDate = new Date();
      * --> checking for and updating the displayed events on the present view.
      */
     void updateGUI() {
-		updateDateLabels(new Date());
+    	System.out.println("updateGUI: Updating GUI...");
+    	
+		updateDateLabels(getSelectedDate());
 		updateUpcomingEvents();
+		updateDayView();
 	}
     
+    void updateDayView() {
+     	// Get events from currentUser that exist within the specified date     	
+     	System.out.println(String.format("%nupdateDayView: Attempting to update day view..."));
+
+     	System.out.println("updateDayView: User has '" + getCurrentUser().getEvents().size() + "' weeks stored in user data.");
+
+     	ArrayList<Week> userEventsTemp = getCurrentUser().getEvents();
+
+     	int weekOfYear = getSelectedDate().get(ChronoField.ALIGNED_WEEK_OF_YEAR);
+     	int dayOfWeek = getSelectedDate().get(ChronoField.DAY_OF_WEEK);
+     	System.out.println("updateDayView: Attempting to get day '" + dayOfWeek + "' of the '" + weekOfYear + "' week of the year...");
+
+     	System.out.println("--> Attempting to get week '" + weekOfYear + "' of the year...");
+
+     	boolean weekExists = false;
+     	for (Week w: userEventsTemp) {
+     		if (w.getWeekNum() == weekOfYear) {
+     			// Week 'weekOfYear' exists in userEvents.
+     			System.out.println("1	--> Week '" + weekOfYear + "' exists in userEvents.");
+     			weekExists = true;
+
+     	    	System.out.println("2	--> Attempting to get day '" + dayOfWeek + "' of the '" + weekOfYear + "' week of the year...");
+     	    	Day dayTemp = w.getDay(dayOfWeek + 1);    
+
+     	    	ArrayList<Event> dayEventsTemp = dayTemp.getEvents();
+
+     	    	System.out.println("3	--> Attempting to get '" + dayEventsTemp.size() + "' events from day '" + dayOfWeek + "' of the '" + weekOfYear + "' week of the year...");
+     	    	for (Event e : dayEventsTemp) {
+     	        	System.out.println("4	--> Event '" + e.getName() + "' found.");
+     	    		dayViewVBox.getChildren().add(generateEventBlock(e));
+     	        	System.out.println("5	--> Created event block for event '" + e.getName() + "' found.");
+     	    	}
+     		}
+     	}
+     	if (!weekExists)
+     		System.out.println("--> Week does NOT exist in user events.");
+
+     	System.out.println("updateDayView: Done.");
+     }
+    
     void updateUpcomingEvents() {
+    	// This should NOT be dependent on 'selectedDate' because it is relative to the current date not the selected date.
     	System.out.println("updateUpcomingEvents: Updating upcomingEventsVbox...");
 
     }
     
-    void updateDateLabels(Date inputDate) {
+    void updateDateLabels(ZonedDateTime inputDate) {
     	System.out.println("updateDateLabels: Updating date labels...");
 
     	String formattedDate = dateLabelFormat.format(inputDate);
@@ -122,4 +181,11 @@ public Date currentDate = new Date();
     	monthDateLabel.setText(formattedDate);
     	
     }
+    
+	static Rectangle generateEventBlock(Event inputEvent) {
+ 		Rectangle eventBlock = new Rectangle(200, 75, inputEvent.getColour());
+
+ 		return eventBlock;
+ 	}
+
 }
